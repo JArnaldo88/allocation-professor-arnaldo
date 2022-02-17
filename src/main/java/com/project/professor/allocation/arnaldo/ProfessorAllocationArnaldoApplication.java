c6a0a14a30164c2a3532b1864d6b722681d7159e
Joao Fontes <joaoarnaldo1@hotmail.com>
2022-02-17 14:57:00 -0300

    commit

---
+++ src/main/java/com/project/professor/allocation/arnaldo/ProfessorAllocationArnaldoApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.arnaldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationArnaldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationArnaldoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/arnaldo/ProfessorAllocationArnaldoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.arnaldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationArnaldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
