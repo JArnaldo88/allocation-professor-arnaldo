dd052b36811716a593e7dff043ae4b6e22a3c0e5
Joao Fontes <joaoarnaldo1@hotmail.com>
2022-02-23 22:22:14 -0300

    Atualização allocation service

--- src/main/java/com/project/professor/allocation/arnaldo/repository/AllocationRepository.java
+++ src/main/java/com/project/professor/allocation/arnaldo/repository/AllocationRepository.java
@@ -1,5 +1,7 @@
 package com.project.professor.allocation.arnaldo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,9 @@ import com.project.professor.allocation.arnaldo.entity.Allocation;
 @Repository
 public interface AllocationRepository extends JpaRepository<Allocation, Long>{
 
+	List<Allocation> findByProfessorId(Long professorId);
+
+    List<Allocation> findByCourseId(Long courseId);
+
+	
 }
